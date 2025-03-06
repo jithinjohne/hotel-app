99f960ebfdeaf74a6c40113258dd348f2cab0518
Jithin John <jithin.john@live.com>
2025-03-06 07:33:07 +0530

    add validation to the input

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validator, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit{
+  
+  reservationForm: FormGroup = this.fromBuilder.group({
+    checkInDate: ['', Validators.required],
+    checkOutDate: ['', Validators.required],
+    guestName: ['', Validators.required],
+    guestEmail: ['', Validators.required],
+    roomNumber: ['', Validators.required]
+  });
+
+  constructor(private fromBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.reservationForm = this.fromBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', Validators.required],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
+
+
+  onSubmit() {
+    if(this.reservationForm.valid) {
+      console.warn(this.reservationForm.value);
+    }
+  }
 
 }
